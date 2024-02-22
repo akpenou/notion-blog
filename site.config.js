ff3efcfdd031f5b354387139e76dfb02a9efd37b
shidoverse <alex.kpenou@gmail.com>
2024-02-22 01:06:59 +0100

    update config

--- site.config.js
+++ site.config.js
@@ -1,25 +1,33 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "shidoverse",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "CEO @ Better People Studio",
+    bio: "Just a developper who like business",
+    email: "alex@betterpeople.studio",
+    linkedin: "akpenou",
+    github: "akpenou",
+    instagram: "_shidoverse_",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Walk With Me`,
+      href: "https://youtube.com/@shidoverse",
     },
+    {
+      name: `Better People`,
+      href: "https://betterpeople.studio",
+    },
+    {
+      name: `Umiros`,
+      href: "https://umiros.com",
+    }
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Walk With Me",
+    description: "Bienvenue sur le blog – walk with me!",
   },
 
   // CONFIG configration (required)
